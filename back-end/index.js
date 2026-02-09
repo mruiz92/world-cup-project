37fd28e26d664e06ac347628b60e35dc25d8caa5
Maria Herreros <127245938+mariaisabelherreros@users.noreply.github.com>
2026-02-09 00:48:50 -0500

    Initial commit

---
+++ back-end/index.js
@@ -0,0 +1,20 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+PORT = 4000;
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:4000",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.listen(PORT, () => console.log("Server running on port " + PORT));
