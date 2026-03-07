19782cbe130d45b7f72f2c7cd9e09566e8c62de6
Sheehan Rahman <sheehanrahman06@gmail.com>
2026-03-07 02:52:21 -0500

    Create placeholder data of users with cards available for trade

---
+++ front-end/src/data/placeholderData.js
@@ -0,0 +1,75 @@
+export const FAKE_DATA = [
+  {
+    id: 1,
+    username: "CANfan",
+    pfp: "https://icons8.com/icons/set/Canada-Flag",
+    tradableCards: [
+      {
+        id: 101,
+        playerName: "Lionel Messi",
+        nationality: "Argentina",
+        rarity: "Legendary",
+        image: "https://madrasafree.net/lionel-messi-2024/",
+      },
+      {
+        id: 102,
+        playerName: "Matt Freese",
+        nationality: "United States",
+        rarity: "Common",
+        image: "https://www.ussoccer.com/players/d/damian-downs",
+      },
+      {
+        id: 103,
+        playerName: "Jarrod Bowen",
+        nationality: "England",
+        rarity: "Uncommon",
+        image: "https://www.bbc.com/sport/extra/yxy7ifkmc7/meet-the-heroes",
+      },
+    ],
+  },
+  {
+    id: 2,
+    username: "USAfan",
+    pfp: "https://icons8.com/icons/set/american-flag",
+    tradableCards: [
+      {
+        id: 201,
+        playerName: "Luis Diaz",
+        nationality: "Colombia",
+        rarity: "Rare",
+        image:
+          "https://www.nytimes.com/athletic/5621652/2024/07/08/luis-diaz-liverpool-colombia-copa-america/",
+      },
+      {
+        id: 202,
+        playerName: "Jarrod Bowen",
+        nationality: "England",
+        rarity: "Uncommon",
+        image: "https://www.bbc.com/sport/extra/yxy7ifkmc7/meet-the-heroes",
+      },
+      {
+        id: 203,
+        playerName: "Christiano Ronaldo",
+        nationality: "Portugal",
+        rarity: "Legendary",
+        image:
+          "https://www.animalia-life.club/qa/pictures/cristiano-ronaldo-png-2022.html",
+      },
+    ],
+  },
+  {
+    id: 3,
+    username: "MEXfan",
+    pfp: "https://icons8.com/icons/set/mexico-flag",
+    tradableCards: [
+      {
+        id: 301,
+        playerName: "James Rodriguez",
+        nationality: "Colombia",
+        rarity: "Epic",
+        image:
+          "https://www.realmadridexclusivo.com/curiosidades/equipo-espanol-esta-paso-fichar-james-rodriguez-regreso-laliga_308624_102.html",
+      },
+    ],
+  },
+];
