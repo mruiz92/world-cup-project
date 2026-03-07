0aceb91083896aa532e9501adc7ad5278f65c16a
Sheehan Rahman <sheehanrahman06@gmail.com>
2026-03-07 03:05:11 -0500

    Auto format

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,23 +1,23 @@
 // In your React component (e.g., App.js)
 
-import React, { useEffect, useState } from 'react';
-import CommunityPage from './pages/CommunityPage';
+import React, { useEffect, useState } from "react";
+import CommunityPage from "./pages/CommunityPage";
 
 function App() {
-  const [message, setMessage] = useState('Not connected');
+  const [message, setMessage] = useState("Not connected");
 
   useEffect(() => {
-    fetch('http://localhost:4000/test-connection')
-      .then(response => response.json())
-      .then(data => setMessage(data.message))
-      .catch(error => console.error('Error fetching data:', error));
+    fetch("http://localhost:4000/test-connection")
+      .then((response) => response.json())
+      .then((data) => setMessage(data.message))
+      .catch((error) => console.error("Error fetching data:", error));
   }, []);
 
   return (
     <div>
       <h1>Connection Status:</h1>
       <p>{message}</p>
-      
+
       <CommunityPage />
     </div>
   );
