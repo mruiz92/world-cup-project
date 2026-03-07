40262b0ba344e6f9cb5370dbfc9db869fc3396d0
Sheehan Rahman <sheehanrahman06@gmail.com>
2026-03-07 03:30:02 -0500

    Implement placeholder data

--- front-end/src/components/UserTradeRow.jsx
+++ front-end/src/components/UserTradeRow.jsx
@@ -1,7 +1,18 @@
-const UserTradeRow = () => {
+const UserTradeRow = ({ user }) => {
   return (
     <div>
-      <p>Placeholder for individual user with cards available for trade</p>
+      <h3>{user.username}</h3>
+      
+      <div>
+        {user.tradableCards.map((card) => (
+          <div key={card.id}>
+            <img src={card.image} alt={card.playerName} />
+            <p>{card.playerName}</p>
+            <p>{card.nationality}</p>
+            <p>{card.rarity}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
