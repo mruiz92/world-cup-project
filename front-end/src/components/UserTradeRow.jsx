161695123317f9ce0ebc7dd1a022705392ca14f5
Sheehan Rahman <sheehanrahman06@gmail.com>
2026-03-07 01:53:37 -0500

    Add skeleton code for community page components

---
+++ front-end/src/components/UserTradeRow.jsx
@@ -0,0 +1,9 @@
+const UserTradeRow = () => {
+  return (
+    <div>
+      <p>Placeholder for individual user with cards available for trade</p>
+    </div>
+  );
+};
+
+export default UserTradeRow;
---
+++ front-end/src/pages/CommunityPage.jsx
@@ -0,0 +1,14 @@
+import UserTradeRow from "../components/UserTradeRow";
+
+const CommunityPage = () => {
+  return (
+    <div>
+      <p>
+        Placeholder for view of rows of users with cards available for trade
+      </p>
+      <UserTradeRow />
+    </div>
+  );
+};
+
+export default CommunityPage;
