5962ee5a0ebbea8ab1d48cf2e4262d3ad54b77de
Sheehan Rahman <sheehanrahman06@gmail.com>
2026-03-11 19:07:14 -0400

    Copy const Card from Register.jsx

--- front-end/src/components/UserTradeRow.jsx
+++ front-end/src/components/UserTradeRow.jsx
@@ -11,6 +11,22 @@ import MuiCard from "@mui/material/Card";
 import { styled } from "@mui/material/styles";
 import { useNavigate } from "react-router-dom";
 
+const TradeRowCard = styled(MuiCard)(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  width: "100%",
+  maxWidth: 450,
+  padding: theme.spacing(4),
+  gap: theme.spacing(2),
+  margin: "auto",
+  boxShadow:
+    "hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px",
+  ...theme.applyStyles("dark", {
+    boxShadow:
+      "hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px",
+  }),
+}));
+
 const UserTradeRow = ({ user }) => {
   return (
     <div>
