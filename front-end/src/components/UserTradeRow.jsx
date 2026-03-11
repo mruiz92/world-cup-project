a8ae85d0be05b176a5709853beeb78be9a93ea95
Sheehan Rahman <sheehanrahman06@gmail.com>
2026-03-11 18:59:20 -0400

    Copy MUI imports from Register.jsx and auto format

--- front-end/src/components/UserTradeRow.jsx
+++ front-end/src/components/UserTradeRow.jsx
@@ -1,8 +1,21 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Divider from "@mui/material/Divider";
+import FormLabel from "@mui/material/FormLabel";
+import FormControl from "@mui/material/FormControl";
+import Link from "@mui/material/Link";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import MuiCard from "@mui/material/Card";
+import { styled } from "@mui/material/styles";
+import { useNavigate } from "react-router-dom";
+
 const UserTradeRow = ({ user }) => {
   return (
     <div>
       <h3>{user.username}</h3>
-      
+
       <div>
         {user.tradableCards.map((card) => (
           <div key={card.id}>
