bf60bb7a226e127d36c5cae1b52d147c6a9b918c
Sheehan Rahman <sheehanrahman06@gmail.com>
2026-03-12 02:01:29 -0400

    Replace barebones logic with MUI components

--- front-end/src/pages/CommunityPage.jsx
+++ front-end/src/pages/CommunityPage.jsx
@@ -1,16 +1,27 @@
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
 import UserTradeRow from "../components/UserTradeRow";
 import { FAKE_DATA } from "../data/placeholderData";
 
 const CommunityPage = () => {
   return (
-    <div>
-      <h1>Community Page</h1>
-      <h2>Available Trades:</h2>
+    <Container maxWidth="md" sx={{ py: 4 }}>
+      <Typography variant="h4" component="h1" gutterBottom fontWeight="bold">
+        Community Page
+      </Typography>
 
-      {FAKE_DATA.map((user) => (
-        <UserTradeRow key={user.id} user={user} />
-      ))}
-    </div>
+      <Typography variant="h6" color="text.secondary" gutterBottom>
+        Available Trades:
+      </Typography>
+
+      <Stack spacing={4} sx={{ mt: 4 }}>
+        {FAKE_DATA.map((user) => (
+          <UserTradeRow key={user.id} user={user} />
+        ))}
+      </Stack>
+    </Container>
   );
 };
 
