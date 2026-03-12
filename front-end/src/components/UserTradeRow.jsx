a37ca924f37661ab73d82aa872ecaee43678c1f6
Sheehan Rahman <sheehanrahman06@gmail.com>
2026-03-12 01:21:49 -0400

    Update imports

--- front-end/src/components/UserTradeRow.jsx
+++ front-end/src/components/UserTradeRow.jsx
@@ -1,17 +1,9 @@
 import * as React from "react";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import Divider from "@mui/material/Divider";
-import FormLabel from "@mui/material/FormLabel";
-import FormControl from "@mui/material/FormControl";
-import Link from "@mui/material/Link";
-import TextField from "@mui/material/TextField";
+import Card from "@mui/material/Card";
 import Typography from "@mui/material/Typography";
-import MuiCard from "@mui/material/Card";
 import { styled } from "@mui/material/styles";
-import { useNavigate } from "react-router-dom";
 
-const TradeRowCard = styled(MuiCard)(({ theme }) => ({
+const TradeRowCard = styled(Card)(({ theme }) => ({
   display: "flex",
   flexDirection: "column",
   width: "100%",
