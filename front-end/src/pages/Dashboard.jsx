a34a23eeaef3ab5ee8df434401c15b7ddc98c142
Kevin Concha <kconcha02@gmail.com>
2026-03-12 18:11:39 -0400

    Update Dashboard.jsx

---
+++ front-end/src/pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Divider from '@mui/material/Divider';
+import FormLabel from '@mui/material/FormLabel';
+import FormControl from '@mui/material/FormControl';
+import Link from '@mui/material/Link';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import MuiCard from '@mui/material/Card';
+import { styled } from '@mui/material/styles';
+import { useNavigate} from 'react-router-dom';
+
