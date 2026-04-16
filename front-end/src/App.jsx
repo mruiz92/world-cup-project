7ccb5ebd338181cb889a3660ec7d87dbb38245ba
Maria Herreros <127245938+mariaisabelherreros@users.noreply.github.com>
2026-04-16 17:16:38 -0400

    Login as Landing Page

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route element={<AppLayout />}>
-            <Route path="/" element={<Navigate to="/register" />} />
+            <Route path="/" element={<Navigate to="/login" />} />
             <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login />} />
             <Route path="/forgot_password" element={<ForgotPassword />} />
